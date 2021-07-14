c275a8f2bab31fcdf4e00e32fca86feaef298894
Paige <ptuttosi@haiven-vm.arbutus>
2021-07-14 05:20:51 +0000

    add proxy pass for nginx to express access

--- api-server/app/routes/appRoutes.js
+++ api-server/app/routes/appRoutes.js
@@ -4,21 +4,17 @@ module.exports = function(app) {
   const alzheimerController = require('../controllers/alzheimerController.js')
   const ukbiobankController = require('../controllers/ukbiobankController.js')
 
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  });
+  app.route('/api/data').get(controller.get_data);
+  app.route('/api/range').get(controller.get_range);
+  app.route('/api/query').get(controller.get_query);
+  app.route('/api/values').get(controller.get_values);
+  app.route('/api/schemas').get(controller.get_schemas);
 
-  app.route('/data').get(controller.get_data);
-  app.route('/range').get(controller.get_range);
-  app.route('/query').get(controller.get_query);
-  app.route('/values').get(controller.get_values);
-  app.route('/schemas').get(controller.get_schemas);
-
-  app.route('/alzheimer/data').get(alzheimerController.get_data);
-  app.route('/alzheimer/range').get(alzheimerController.get_range);
-  app.route('/alzheimer/query').get(alzheimerController.get_query);
+  app.route('/api/alzheimer/data').get(alzheimerController.get_data);
+  app.route('/api/alzheimer/range').get(alzheimerController.get_range);
+  app.route('/api/alzheimer/query').get(alzheimerController.get_query);
   
-  app.route('/ukbiobank/data').get(ukbiobankController.get_data);
-  app.route('/ukbiobank/range').get(ukbiobankController.get_range);
-  app.route('/ukbiobank/query').get(ukbiobankController.get_query);
+  app.route('/api/ukbiobank/data').get(ukbiobankController.get_data);
+  app.route('/api/ukbiobank/range').get(ukbiobankController.get_range);
+  app.route('/api/ukbiobank/query').get(ukbiobankController.get_query);
 }
